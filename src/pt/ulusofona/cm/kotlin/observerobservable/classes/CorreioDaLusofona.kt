836a555561e8243aea609d52ff91a55026dcd36a
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-05-07 19:29:23 +0100

    DONE ?

--- src/pt/ulusofona/cm/kotlin/observerobservable/classes/CorreioDaLusofona.kt
+++ src/pt/ulusofona/cm/kotlin/observerobservable/classes/CorreioDaLusofona.kt
@@ -6,15 +6,23 @@ class CorreioDaLusofona(var maxLeitores : Int, private var noticias : MutableLis
     private lateinit var listeners : MutableList<OnNoticiaListener>
 
     fun adicionarLeitor(leitor : OnNoticiaListener) {
-
+        if (listeners.size < maxLeitores) {
+            leitor.leitorAdicionadoComSucesso()
+            listeners.add(leitor)
+        }
     }
 
     fun removerLeitor(leitor: OnNoticiaListener) {
-
+        leitor.leitorRemovidoComSucesso()
+        listeners.remove(leitor)
     }
 
     private fun notificarLeitores() {
-
+        for (leitor in listeners) {
+            for (noticia in noticias) {
+                leitor.onReceiveNoticia(noticia)
+            }
+        }
     }
 
     fun iniciar() {
--- src/pt/ulusofona/cm/kotlin/observerobservable/classes/GeradorNumerico.kt
+++ src/pt/ulusofona/cm/kotlin/observerobservable/classes/GeradorNumerico.kt
@@ -2,19 +2,27 @@ package pt.ulusofona.cm.kotlin.observerobservable.classes
 
 import pt.ulusofona.cm.kotlin.observerobservable.interfaces.OnNumeroListener
 
-class GeradorNumerico(var maxLeitores : Int, private var noticias : MutableList<Int>) {
+class GeradorNumerico(var maxLeitores : Int, private var numeros : MutableList<Int>) {
     private lateinit var listeners : MutableList<OnNumeroListener>
 
     fun adicionarLeitor(leitor : OnNumeroListener) {
-
+        if (listeners.size < maxLeitores) {
+            leitor.leitorAdicionadoComSucesso()
+            listeners.add(leitor)
+        }
     }
 
     fun removerLeitor(leitor: OnNumeroListener) {
-
+        leitor.leitorRemovidoComSucesso()
+        listeners.remove(leitor)
     }
 
     private fun notificarLeitores() {
-
+        for (leitor in listeners) {
+            for (numero in numeros) {
+                leitor.onReceiveNumero(numero)
+            }
+        }
     }
 
     fun iniciar() {
